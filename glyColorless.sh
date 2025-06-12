e55f2892c154b1f8fd26a7db5f54e8c1524050b0
Thomas <ts16042005@gmail.com>
2025-06-12 09:35:05 +0200

    if format + alias

--- glyColorless.sh
+++ glyColorless.sh
@@ -1,18 +1,18 @@
 #!/bin/zsh
 source ~/.zshrc
-OUTPUT=$(gl)
+OUTPUT=$("Alias de votre commande. Ex : 'gl'")
 GLYCEMIA=$(echo "$OUTPUT" | grep -oP '\d+(\.\d+)?')
 SUPERLOW=40
 LOW=70  
 HIGH=180 
 SUPERHIGH=400
-if (( $(echo "$GLYCEMIA < $SUPERLOW" | bc -l) )); then
+if (( $GLYCEMIA < $SUPERLOW )); then
     COLOR="#fd84ff"
-elif (( $(echo "$GLYCEMIA < $LOW" | bc -l) )); then
+elif (( $GLYCEMIA < $LOW )); then
     COLOR="#f38ba8"
-elif (( $(echo "$GLYCEMIA > $SUPERHIGH" | bc -l) )); then
+elif (( $GLYCEMIA > $SUPERHIGH )); then
     COLOR="#f5c2e7"
-elif (( $(echo "$GLYCEMIA > $HIGH" | bc -l) )); then
+elif (( $GLYCEMIA > $HIGH )); then
     COLOR="#f9e2af" 
 else
     COLOR="#94e2d5"
